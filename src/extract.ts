706c7d801aab910fd5e39af423ef81b81196831f
creativeduck <creativeduck0518@gmail.com>
2023-07-21 10:32:44 +0900

    refactor

--- src/extract.ts
+++ src/extract.ts
@@ -24,10 +24,6 @@ async function getReview(
     orderBy: orderBy,
   })
 
-  if (response.data.result !== 'ok') {
-    throw new Error()
-  }
-
   const data = response.data.data.data
   const dataTotalCount = response.data.dataTotalCount
 
@@ -38,50 +34,21 @@ async function getReview(
 }
 
 export default async function extract(url: string, lastReviewId?: number) {
+  const results: ReviewResult[] = []
   const urlObj = new URL(url)
   const host = urlObj.host
   const urls = urlObj.pathname.split('/')
   const mgCode = urls[urls.length - 1]
-  if (!mgCode) {
-    throw new Error()
-  }
   let page = 1
+  let itemCount = 0
   let done = false
-  const { data, dataTotalCount } = await getReview(host, page, mgCode)
-  const results: ReviewResult[] = []
-
-  for (const review of data) {
-    if (review.c_idx === lastReviewId) {
-      done = true
-      break
-    }
-
-    results.push({
-      reviewId: review.c_idx,
-      message: review.c_content.trim(),
-      writer: review.c_name,
-      optionValue: '',
-      date: review.c_regdate,
-      rate: review.c_grade,
-      images: review.photoList.split('~|~').filter(photo => photo.length > 0),
-    })
-  }
-
-  if (done) {
-    return results
-  }
-
-  let itemCount = results.length
+  const { dataTotalCount } = await getReview(host, page, mgCode)
   const max = lastReviewId ? dataTotalCount : Math.min(dataTotalCount, 200)
-  while (!done && itemCount < max) {
-    const { data } = await getReview(host, ++page, mgCode)
-    for (const review of data) {
-      if (review.c_idx === lastReviewId) {
-        done = true
-        break
-      }
 
-      results.push({
+  while (!done && itemCount < max) {
+    const { data } = await getReview(host, page++, mgCode)
+    const reviews = data.map(review => {
+      return {
         reviewId: review.c_idx,
         message: review.c_content.trim(),
         writer: review.c_name,
@@ -89,8 +56,17 @@ export default async function extract(url: string, lastReviewId?: number) {
         date: review.c_regdate,
         rate: review.c_grade,
         images: review.photoList.split('~|~').filter(photo => photo.length > 0),
-      })
-      itemCount++
+      }
+    })
+    const findIndex = reviews.findIndex(
+      review => review.reviewId === lastReviewId
+    )
+    if (findIndex !== -1) {
+      results.push(...reviews.slice(0, findIndex))
+      done = true
+    } else {
+      results.push(...reviews)
+      itemCount += 5
     }
   }
 
