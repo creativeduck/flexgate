ccfe334778bd87c2cb6e5af2467d5c8822423abd
creativeduck <creativeduck0518@gmail.com>
2023-07-24 13:48:51 +0900

    refactor blogpay

--- src/blogpay.ts
+++ src/blogpay.ts
@@ -1,127 +1,78 @@
-import puppeteer from 'puppeteer'
-import { Browser } from 'puppeteer'
+import axios from 'axios'
+import { load } from 'cheerio'
 import { ReviewResult } from './type'
-
-let _browser: Browser | null = null
-
-async function getBrowser() {
-  if (!_browser) {
-    _browser = await puppeteer.launch({
-      headless: false,
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    })
-  }
-
-  return _browser
-}
-
-async function resetBrowser() {
-  if (_browser) {
-    _browser.close()
-    _browser = null
-  }
-}
 
 function getUrl(host: string, goodNum: string, pageNum: number) {
   return `https://${host}/good/product_view_goodrate_list?goodNum=${goodNum}&page=${pageNum}`
 }
 
 export async function extract(url: string, lastReviewId?: string) {
-  lastReviewId
-
+  const results: ReviewResult[] = []
   const urlObj = new URL(url)
   const host = urlObj.host
   const goodNum = urlObj.searchParams.get('goodNum')
   if (!goodNum) {
-    return []
+    return results
   }
   let pageNum = 1
-  let browser = null
-  let page = null
-  const results = []
 
-  try {
-    do {
-      browser = await getBrowser()
-      page = await browser.newPage()
+  do {
+    const { data } = await axios.get<string>(getUrl(host, goodNum, pageNum++))
+    const $ = load(data)
+    const childList = $('div.prod-review-item').toArray()
 
-      await page.goto(getUrl(host, goodNum, pageNum++))
-
-      const reviews = await page.evaluate(() => {
-        const childList = Array.from(
-          document.querySelectorAll('div.prod-review-item')
-        )
-
-        if (childList) {
-          return childList.map(review => {
-            const reviewId = review?.getAttribute('bbsidx') || ''
-            const rate = Number(
-              review.querySelector(
-                'div.prod-review-rating > div.star-rating-wrap > strong'
-              )?.textContent || ''
-            )
-            const writer =
-              review.querySelector('div.prod-review-info > div > div > em')
-                ?.textContent || ''
-            const date =
-              review.querySelector(
-                'div.prod-review-info > div > div.prod-review-date > span'
-              )?.textContent || ''
-            const first =
-              review.querySelector(
-                'div.prod-review-info > div.prod-review-txt > p'
-              )?.textContent || ''
-            const second =
-              review.querySelector('div.prod-review-detail')?.textContent || ''
-            const message = `${first}\n${second}`.trim()
-            const images = [
-              (
-                review.querySelector(
-                  'div.review-imgwrap > img'
-                ) as HTMLImageElement
-              )?.src || '',
-            ]
-            const optionValue = ''
-            const result: ReviewResult = {
-              reviewId,
-              message,
-              writer,
-              optionValue,
-              date,
-              rate,
-              images,
-            }
-
-            return result
-          })
-        }
-        return []
-      })
-
-      if (!reviews.length) {
-        break
-      }
-
-      const findIndex = reviews.findIndex(
-        review => review.reviewId === lastReviewId
+    const reviews = childList.map(review => {
+      const reviewId = review.attribs.bbsidx
+      const rate = Number(
+        $(review)
+          .find('div.prod-review-rating > div.star-rating-wrap > strong')
+          .text()
       )
-      if (findIndex !== -1) {
-        results.push(reviews.slice(0, findIndex))
-        break
-      } else {
-        results.push(reviews)
-      }
-    } while (results.length < 200)
+      const writer = $(review)
+        .find('div.prod-review-info > div > div > em')
+        .text()
+      const date = $(review)
+        .find('div.prod-review-info > div > div.prod-review-date > span')
+        .text()
+      const firstContent = $(review)
+        .find('div.prod-review-info > div.prod-review-txt > p')
+        .text()
+        .trim()
+      const secondContent = $(review)
+        .find('div.prod-review-detail')
+        .text()
+        .trim()
+      const message = `${firstContent}\n${secondContent}`
+      const images = [...$(review).find('div.review-imgwrap > img')].map(
+        img => `https://${host}${img.attribs['src']}`
+      )
+      const optionValue = ''
 
-    return results
-  } catch (err) {
-    if (browser) {
-      await resetBrowser()
+      return {
+        reviewId,
+        message,
+        writer,
+        optionValue,
+        date,
+        rate,
+        images,
+      }
+    })
+
+    if (!reviews.length) {
+      break
     }
-    console.log(err)
-    return []
-  } finally {
-    if (page) await page.close()
-    if (browser) await resetBrowser()
-  }
+
+    const findIndex = reviews.findIndex(
+      review => review.reviewId === lastReviewId
+    )
+    if (findIndex !== -1) {
+      results.push(...reviews.slice(0, findIndex))
+      break
+    } else {
+      results.push(...reviews)
+    }
+  } while (results.length < 200)
+
+  return results
 }
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { extract } from './blogpay'
 ;(async () => {
   const url =
-    'https://jinsun1576.shop.blogpay.co.kr/good/product_view?goodNum=202835996'
-  const lastReviewId = '200600394'
+    'https://polehigh50.shop.blogpay.co.kr/good/product_view?goodNum=203433566'
+  const lastReviewId = '200675562'
   const results = await extract(url, lastReviewId)
 
   console.log(results)
